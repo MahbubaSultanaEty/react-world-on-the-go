f16fdbf9ed523a83da8635881c0578f16fb90b9e
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-24 15:06:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+        
+        <div>
+          <h1>React World on the go</h1>
+          <Countries></Countries>
+        </div>
+    
+      </section>     
+
+    
+
+     
+     
+    </>
+  )
+}
+
+export default App
