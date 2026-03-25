4ace4c19497509d7c731b38ef93b32f8c9e4d3ba
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-25 21:50:21 +0600

    conditional css and toggle button Based on click using setState()

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css';
 
 
 const Country = ({ country }) => {
     // console.log(country.name.common)
 
+    const [visited, setVisited] = useState(false);
+
     const handleVisited = () => {
-        console.log("button clicked")
+        // method 1:
+        // if (visited) {
+        //     setVisited(false)
+        // }
+        // else {
+        //     setVisited(true)
+        // }
+
+        // method 2:
+        // setVisited(true)
+
+        // method 3:
+        // setVisited(visited ? false : true);
+
+        // method 4: 
+        setVisited(!visited)
+
+
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && "country-visited"}`}>
             <img src={country.flags?.flags.png} alt={country.flags.flags.alt } />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
             <p>Capital: {country.capital.capital}</p>
             <p>Continents: {country.continents.continents}</p>
             <p>Area : {country.area.area} {country.area.area > 300000 ? "Big country" : "small country"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>{ visited? "visited": "not visited"}</button>
         </div>
     );
 };
